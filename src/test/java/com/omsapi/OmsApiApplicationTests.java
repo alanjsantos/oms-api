d0b5e18ca047e3627aa200111184b1d47f7048b6
Alan <alanjosedossantos011@gmail.com>
2021-09-30 17:43:21 -0300

    first commit

---
+++ src/test/java/com/omsapi/OmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
