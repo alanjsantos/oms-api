700e94e8f964889951da30cb198eb46dfaa813b2
Alan <alanjosedossantos011@gmail.com>
2021-10-05 14:53:48 -0300

    implementando metodos em repository

--- src/main/java/com/omsapi/repository/RequestRepository.java
+++ src/main/java/com/omsapi/repository/RequestRepository.java
@@ -1,9 +1,19 @@
 package com.omsapi.repository;
 
 import com.omsapi.models.Request;
+import com.omsapi.models.enums.RequestState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    List<Request> findAllByOwnerId(Long id);
+
+    @Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+    Request updateStatus(Long id, RequestState state);
 }
+
